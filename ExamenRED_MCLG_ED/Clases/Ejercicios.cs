25dbf7bb1b390894681dfc79234f2597111206da
MariaLimon <limon185mari@gmail.com>
2023-11-10 07:51:06 -0700

    first commit

---
+++ ExamenRED_MCLG_ED/Clases/Ejercicios.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenRED_MCLG_ED.Clases
+{
+    internal class Elercicios
+    {
+    }
+}
